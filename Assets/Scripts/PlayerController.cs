c629c4e1dbe005158375aa0e9891fcfb6992f4a7
proud <theproud.business@gmail.com>
2022-07-21 14:39:20 +0300

    V 0.0.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,31 @@ public class PlayerController : MonoBehaviour
     private float movementInputDirection;
     private float movementSpeed = 10.5f;
     private float jumpForce = 16.0f;
+
+    private int amountOfJumpsLeft;
+
     private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+
+    private Animator anim;
     private Rigidbody2D rb;
 
+    public float groundCheckRadius;
+
+    public int amountOfJumps;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsMask;
     // Start is called before the first frame update
     private void Start()
     {
         // TODO: CHANGE NAME
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -20,18 +37,32 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        CheckIfCanJump();
+        UpdateAnimations();
     }
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
     }
     private void ApplyMovement()
     {
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0.01f)
+            amountOfJumpsLeft = amountOfJumps;
+
+        if (amountOfJumpsLeft<=0)
+            canJump = false;
+        else
+            canJump = true;
+    }
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
     private void CheckInput()
     {
@@ -39,7 +70,17 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
+            amountOfJumpsLeft--;
         }
+    }private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsMask);
+    }
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
     private void CheckMovementDirection()
     {
@@ -51,10 +92,24 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+        if (rb.velocity.x!=0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
     private void Flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        //Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
     }
 }
